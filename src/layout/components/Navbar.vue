d16447fac0a2be08d899a2ba4115348dd60fe9c7
1404031711@qq.com <1404031711@qq.com>
2021-02-28 16:00:35 +0800

    消除github链接

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -17,7 +17,7 @@
             </el-dropdown-item>
           </router-link>
           <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
-            <el-dropdown-item>Github</el-dropdown-item>
+            <!-- <el-dropdown-item>Github</el-dropdown-item> -->
           </a>
           <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
             <el-dropdown-item>Docs</el-dropdown-item>
